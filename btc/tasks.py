7c171a60ddfa621cdb0ed392083d748119cfa57e
Tsatsu Adogla-Bessa Jnr <tsatsujnr@gmail.com>
2021-05-06 03:43:14 +0000

    clean up btc schduled task

--- btc/tasks.py
+++ btc/tasks.py
@@ -1,6 +1,7 @@
 from core.celery import app
+from celery.schedules import crontab
 from celery.utils.log import get_task_logger
-from .utils import get_current_btc_exchange_rate
+from .utils import get_current_btc_exchange_rate, save_or_update_current_exchange_rate
 
 logger = get_task_logger(__name__)
 
@@ -8,14 +9,13 @@ logger = get_task_logger(__name__)
 @app.task
 def get_current_btc_exchange_rate_task():
     data = get_current_btc_exchange_rate()
-    save_current_exchange_rate(data)
-    return data
+    save_or_update_current_exchange_rate(data)
 
 
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     # executes every hour
     sender.add_periodic_task(
-        crontab(hour='*', minute=0),
+        crontab(hour="*"),
         get_current_btc_exchange_rate_task.s(),
     )
